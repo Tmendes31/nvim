1459c78e69f14510361c576eb4e29e9c8c849d53
Tomás Mendes <tomasmendes19@gmail.com>
2025-08-23 22:37:00 +0100

    changed colors for git status

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -25,7 +25,7 @@ return {
         })
 
         -- highlight overrides
-        vim.api.nvim_set_hl(0, "NvimTreeGitNew", { fg = "#5c6370" })
+        vim.api.nvim_set_hl(0, "NvimTreeGitNew", { fg = "#41FDFE" })
         vim.api.nvim_set_hl(0, "NvimTreeGitDirty", { fg = "#e5c07b" })
         vim.api.nvim_set_hl(0, "NvimTreeGitStaged", { fg = "#98c379" })
         vim.api.nvim_set_hl(0, "NvimTreeGitDeleted", { fg = "#e06c75" })
